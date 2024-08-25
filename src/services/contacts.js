273932d93f47efdf755277c7e5c0a1c012115071
AgentGarrus <spektr589@gmail.com>
2024-08-25 15:40:27 +0200

    hw4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,46 @@
 import { ContactsCollection } from "../db/models/contacts.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constants/index.js";
 
-export const getAllContacts = () => ContactsCollection.find();
+
+export const getAllContacts = async ({
+    page = 1,
+    perPage = 10,
+    sortOrder = SORT_ORDER.ASC,
+    sortBy = "_id",
+    filter = {},
+}) => {
+    const limit = perPage;
+    const skip = page > 0 ? (page - 1) * perPage : 0;
+
+    const contactsQuery = ContactsCollection.find();
+
+    if (typeof filter.type !== "undefined") {
+        contactsQuery.where("contactType").equals(filter.type);
+    }
+    if (typeof filter.isFavorite !== "undefined") {
+        contactsQuery.where("isFavorite").equals(filter.isFavorite);
+    }
+
+    const [contactsCount, contacts] = await Promise.all([
+        ContactsCollection.find()
+            .merge(contactsQuery)
+            .countDocuments(),
+        contactsQuery
+            .skip(skip)
+            .limit(limit)
+            .sort({ [sortBy]: sortOrder })
+            .exec(),
+    ]);
+
+
+    const paginationData = calculatePaginationData(contactsCount, perPage, page);
+    return {
+        data: contacts,
+        ...paginationData,
+    };
+
+};
 
 export const getContactsById = (contactId) => ContactsCollection.findById(contactId);
 
@@ -10,7 +50,7 @@ export const createContact = (payload) => {
 
 export const updateContact = (contactId, payload) => {
     return ContactsCollection.findByIdAndUpdate(
-        contactId, payload, { new: true });
+        contactId, payload, { new: true }); // чи буде повернуто оновлений документ //
 };
 
 export const deleteContact = (contactId) => {
