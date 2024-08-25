cf8b7a0699365cbf09a125e7feb6da043eaae8c5
AgentGarrus <spektr589@gmail.com>
2024-08-25 15:32:51 +0200

    hw3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,66 +1,20 @@
 import { ContactsCollection } from "../db/models/contacts.js";
-import { calculatePaginationData } from "../utils/calculatePaginationData.js";
-import { SORT_ORDER } from "../constants/index.js";
 
+export const getAllContacts = () => ContactsCollection.find();
 
-export const getAllContacts = async ({
-    page = 1,
-    perPage = 10,
-    sortOrder = SORT_ORDER.ASC,
-    sortBy = "_id",
-    filter = {},
-    userId,
-}) => {
-    const limit = perPage;
-    const skip = page > 0 ? (page - 1) * perPage : 0;
-
-    const contactsQuery = ContactsCollection.find();
-
-    if (typeof filter.type !== "undefined") {
-        contactsQuery.where("contactType").equals(filter.type);
-    }
-    if (typeof filter.isFavorite !== "undefined") {
-        contactsQuery.where("isFavorite").equals(filter.isFavorite);
-    }
-
-    contactsQuery.where("userId").equals(userId);
-
-    const [contactsCount, contacts] = await Promise.all([
-        ContactsCollection.find()
-            .merge(contactsQuery)
-            .countDocuments(),
-        contactsQuery
-            .skip(skip)
-            .limit(limit)
-            .sort({ [sortBy]: sortOrder })
-            .exec(),
-    ]);
-
-
-    const paginationData = calculatePaginationData(contactsCount, perPage, page);
-    return {
-        data: contacts,
-        ...paginationData,
-    };
-
-};
-
-export const getContactsById = (contactId, userId) => ContactsCollection.findOne({ _id: contactId, userId });
+export const getContactsById = (contactId) => ContactsCollection.findById(contactId);
 
 export const createContact = (payload) => {
     return ContactsCollection.create(payload);
 };
 
-export const updateContact = (contactId, payload, userId) => {
-    return ContactsCollection.findOneAndUpdate(
-        { _id: contactId, userId },
-        payload,
-        { new: true });
+export const updateContact = (contactId, payload) => {
+    return ContactsCollection.findByIdAndUpdate(
+        contactId, payload, { new: true });
 };
 
-export const deleteContact = (contactId, userId) => {
+export const deleteContact = (contactId) => {
     return ContactsCollection.findOneAndDelete({
         _id: contactId,
-        userId,
     });
 };
